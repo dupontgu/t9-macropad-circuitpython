dfb981fa39a94ec6d81c50dd6af23a22de048ece
Guy Dupont <gvy.dvpont@gmail.com>
2022-04-16 14:36:24 -0400

    add multi-language support, update docs

--- fw/README.md
+++ fw/README.md
@@ -6,8 +6,6 @@ As of May 2021, it's only been tested on various RP2040 dev boards.
 Board-agnostic code lives in the [core](./core) directory. 
 
 Board-specific code - for things like driving indicator LEDs, GPIO mappings - are in other folders. 
-Word libraries are also in these individual directories for now, as certain dev boards have less flash space.
-Those boards have smaller default libraries.
 Currently supported boards:
 * [Pimoroni Tiny2040](./tiny2040)
 * [Raspberry Pi Pico](./pico)
@@ -23,12 +21,13 @@ tested natively on desktop. *For key press detection on Mac, you'll need to give
 2. Find the directory with your board's name in the `bundle` directory. Copy all files/subdirectories onto your CircuitPython device.
 3. If your board requires third-party libraries to run this code (most do), you may have to manually copy them. Each board's `bundle` directory will contain a README listing all of the required dependencies. Official CircuitPython libraries available [here](https://circuitpython.org/libraries). As of May 2021, I have been using Bundle version 6.x.
 4. If you're using one of the desktop versions, execute `python3 code.py` from the `bundle/[macos/win32]` directory. You'll have to `pip3 install pynput` as well.
+5. There is now a `test` target that should run on any machine. This is just a script that simulates the typing of a single word. Execute `python3 code.py` from the `bundle/test` directory to run.
 
 ## Customizing the keypad layout
 
-You can customize which letters get attached to each key by changing the `keypad_dict` dictionary in `code.py`. The default is:
+You can customize which letters get attached to each key by changing the `key_map` dictionary in `key_map.py`. The default for English is something like:
 ```python
-keypad_dict = {
+key_map = {
     '1' : ['1'],
     '2' : ['a', 'b', 'c'],
     '3' : ['d', 'e', 'f'],
@@ -44,7 +43,7 @@ keypad_dict = {
 ```
 But as [Ben Torvaney discovered](https://torvaney.github.io/projects/t9-optimised.html), you can optimize your typing even further by using something like:
 ```python
-keypad_dict = {
+key_map = {
     '1' : ['1'],
     '2' : ['a', 'm', 'r'],
     '3' : ['c', 'd', 'f', 'p', 'u'],
--- library_generator/src/main/kotlin/com/dupontgu/t9/Main.kt
+++ library_generator/src/main/kotlin/com/dupontgu/t9/Main.kt
@@ -1,301 +1,87 @@
+import com.dupontgu.t9.*
+import org.apache.commons.text.StringEscapeUtils
+import java.io.BufferedInputStream
 import java.io.File
 import java.io.FileOutputStream
-import java.io.InputStream
-import java.io.OutputStream
-import java.util.*
+import java.net.URL
 
-// Each pointer is a 24 bit address
-private const val ADDRESS_LENGTH_BYTES = 3
-// Each node will have a header with "metadata"
-private const val HEADER_LENGTH_WORDS = 3
-private const val HEADER_LENGTH_BYTES = HEADER_LENGTH_WORDS * ADDRESS_LENGTH_BYTES
+private const val OUT_DIR = "output"
+private const val GLOBAL_CHAR_MAP_FILE = "character_map.py"
+const val KEY_MAP_FILE = "key_map.py"
 
-// newline delimited list of words for your T9 dictionary
-private const val DICT_FILE = "/dict.txt"
-
-// where the custom library file will be written
-private const val OUT_FILE = "library.t9l2"
-
-// a-z
-private const val ALPHA_COUNT = 26
-
-// helpers to check if a library entry matches our supported alphabet
-private val alphabetRegex = Regex("^[a-zA-Z]*\$")
-private fun String.isValid() = matches(alphabetRegex)
-
-private const val MAX_LINE_LEN = 25
-private fun String.hasValidLength() = length <= MAX_LINE_LEN
-
-sealed class LibraryResult : Throwable() {
-    data class Success(val numBytesWritten: Long) : LibraryResult()
-    sealed class Error(override val message: String) : LibraryResult() {
-        data class LineTooLongError(val line: String) : Error("Error: this file contains a line that is too long: ${line.take(25)}")
-        data class InvalidLineError(val line: String) : Error("Error: this file contains a line that contains invalid characters: ${line.take(25)}")
-    }
-}
-
-fun serializeLibraryFile(input: InputStream, output: OutputStream): LibraryResult {
-    val tree = try {
-        input
-            .bufferedReader()
-            .lineSequence()
-            // clean each line
-            .map { line ->
-                if (!line.isValid()) throw LibraryResult.Error.InvalidLineError(line)
-                if (!line.hasValidLength()) throw LibraryResult.Error.LineTooLongError(line)
-                line.trim().lowercase(Locale.getDefault()).filter { it.isLetter() }
-            }
-            // insert each word into the Trie
-            .fold<String, TrieNode>(NullNode) { acc, s -> acc.insert(s) }
-    } catch (err: LibraryResult.Error) {
-        return err
-    }
-
-    // serialize the tree
-    val queue = ArrayDeque<TrieNode>().apply { addFirst(tree) }
-
-    var offset = queue.peekFirst().nodeSize.toUInt()
-    output.use { stream ->
-        while (!queue.isEmpty()) {
-            val node = queue.pollFirst()
-            val size = node.serializeTo(offset, queue) { stream.write(it.toByteArray()) }
-            offset += size
-        }
-    }
-
-    return LibraryResult.Success((offset.toLong() * ADDRESS_LENGTH_BYTES))
-}
-
+/**
+ * Downloads a default list of words for each of the supported [Languages],
+ * and generates T9 library files and accompanying key map files.
+ *
+ * Also generates the global character map file, which is used directly by the firmware.
+ */
 fun main() {
-    val input = {}.javaClass.getResource(DICT_FILE).openStream()
-    val output = FileOutputStream(File(OUT_FILE))
-    val result = serializeLibraryFile(input, output)
-    println(
+    val outputDir = File(OUT_DIR).apply {
+        if (!exists()) {
+            mkdir()
+        }
+    }
+
+    Languages.values().forEach {
+        val languageDir = File(outputDir, it.languageCode).apply {
+            if (!exists()) {
+                mkdir()
+            }
+        }
+        val tempFile = kotlin.io.path.createTempFile("library-${it.languageCode}").toFile()
+        URL(it.librarySourceUrl).downloadFileTo(tempFile)
+        val libraryOutputFile = File(languageDir, "library.t9l2")
+        val result = serializeLibraryFile(
+            tempFile.inputStream(),
+            FileOutputStream(libraryOutputFile),
+            removeInvalidLines = true
+        )
         when (result) {
-            is LibraryResult.Success -> "Wrote to file $OUT_FILE, size: ${result.numBytesWritten} bytes"
-            is LibraryResult.Error.LineTooLongError -> "ERROR! Line was too long: ${result.line.take(25)}(...)"
-            is LibraryResult.Error.InvalidLineError -> "ERROR! Line contains invalid characters: ${result.line.take(25)}(...)"
-        }
-    )
-}
-
-class NodeHeader(
-    private val nodeIsWord: Boolean
-) {
-    private val flags = BitSet()
-    fun serializeTo(output: SerializedOutput) {
-        val bytes = MutableList<Byte>(HEADER_LENGTH_BYTES) { 0 }
-        bytes[0] = if(nodeIsWord) 1 else 0
-        var i = HEADER_LENGTH_BYTES - 1
-        flags.toByteArray().forEach { bytes[i--] = it }
-        output.write(bytes)
-    }
-
-    operator fun set(index: Int, value: Boolean) {
-        flags[index] = value
-    }
-}
-
-fun interface SerializedOutput {
-    fun write(bytes: Collection<Byte>)
-}
-
-sealed class TrieNode {
-    abstract fun fetch(char: Char): TrieNode
-
-    private fun fetch(string: String): TrieNode {
-        if (string.isEmpty()) return this
-        return when (val next = fetch(string.first())) {
-            is ParentNode -> next.fetch(string.rest)
-            is OnlyWordNode -> if (string.length > 1) {
-                NullNode
-            } else {
-                OnlyWordNode
+            is LibraryResult.Success -> {
+                println(
+                    """
+                    Wrote to file $libraryOutputFile, size: ${result.numBytesWritten} bytes
+                    ${result.charSet} (${result.charSet.size})
+                    skipped: ${result.skippedWords} 
+                    """.trimIndent()
+                )
+                writeKeyMapFile(result, File(languageDir, KEY_MAP_FILE))
             }
-            is NullNode -> NullNode
+            is LibraryResult.Error.LineTooLongError -> println("ERROR! Line was too long: ${result.line.take(25)}")
+            is LibraryResult.Error.InvalidLineError -> println(
+                "ERROR! Line contains invalid characters: ${result.line.take(25)}"
+            )
         }
+        println()
     }
 
-    abstract fun insert(string: String): TrieNode
-
-    fun contains(string: String): Boolean {
-        return when (fetch(string)) {
-            is WordParentNode -> true
-            is OnlyParentNode -> false
-            is OnlyWordNode -> true
-            is NullNode -> false
-        }
-    }
-
-    open fun serializeTo(offset: UInt, queue: Queue<TrieNode>, output: SerializedOutput): UInt {
-        return 0u
-    }
-
-    abstract val nodeSize: Int
-}
-
-val Char.alphaIndex: Int
-    get() {
-        val alpha = this - 'a'
-        if (alpha < 0) error("unable to insert: $this")
-        return alpha
-    }
-
-val String.rest: String
-    get() = removeRange(0, 1)
-
-/**
- * Base class for nodes that have children
- * In the context of the dictionary trie - a node that represents a partial word
- */
-sealed class ParentNode(
-    protected val chars: MutableList<TrieNode> = MutableList(ALPHA_COUNT) { NullNode }
-) : TrieNode() {
-
-    override fun fetch(char: Char): TrieNode {
-        return chars.getOrElse(char.alphaIndex) { NullNode }
-    }
-
-    fun insertChar(char: Char) {
-        chars[char.alphaIndex] = OnlyWordNode
-    }
-
-    abstract val name: String
-    abstract val isWord: Boolean
-
-    override fun toString(): String {
-        return "$name(\n" +
-                chars.joinToString(separator = "\n") { s ->
-                    s.toString().lines().joinToString("\n") { "  $it" }
-                } +
-                "\n)"
-    }
-
-    private val populatedChars
-        get() = chars.count { it !is NullNode }
-
-    override val nodeSize: Int
-        get() = populatedChars + HEADER_LENGTH_WORDS
-
-    override fun serializeTo(offset: UInt, queue: Queue<TrieNode>, output: SerializedOutput): UInt {
-        val header = NodeHeader(isWord)
-        val sizes = chars.scan(0u) { acc, trieNode -> acc + trieNode.nodeSize.toUInt() }
-        val thisRow = chars.flatMapIndexed { index, node ->
-            val address = when (node) {
-                is ParentNode -> (offset + sizes[index])
-                is OnlyWordNode -> 0xFFFFFFu
-                is NullNode -> null
+    /**
+     * Write global character_map Python file, which can be loaded by T9 FW
+     * The character_map is provides the index of each character's flag within a node header's bitset
+     */
+    File(outputDir, GLOBAL_CHAR_MAP_FILE).outputStream().bufferedWriter().use { writer ->
+        val mappings = charMap
+            .map {
+                // take care of any characters that might not get written to a string properly
+                val unescapedKey = StringEscapeUtils.escapeEcmaScript(it.key.toString())
+                "\"$unescapedKey\" : ${it.value}"
             }
-            header[index] = address != null
-            return@flatMapIndexed address?.toBytes().orEmpty()
-        }
-        queue.addAll(chars)
-        header.serializeTo(output)
-        output.write(thisRow)
-        return sizes.last()
+            .joinToString(",")
+        writer.write("character_map = {$mappings}")
     }
 }
 
-/**
- * A trie node that represents a valid word, and is also the prefix for at least one other word
- */
-class WordParentNode(
-    chars: MutableList<TrieNode> = MutableList(ALPHA_COUNT) { NullNode }
-) : ParentNode(chars) {
-    override fun insert(string: String): TrieNode {
-        if (string.isNotEmpty()) {
-            val first = string.first()
-            chars[first.alphaIndex] = fetch(first).insert(string.rest)
-        }
-        return this
-    }
-
-    override val name: String = "WordParent"
-
-    override val isWord: Boolean = true
-}
-
-/**
- * A trie node that represents a prefix for at least one other word
- */
-class OnlyParentNode(
-    chars: MutableList<TrieNode> = MutableList(ALPHA_COUNT) { NullNode }
-) : ParentNode(chars) {
-    override fun insert(string: String): TrieNode {
-        if (string.isNotEmpty()) {
-            val first = string.first()
-            chars[first.alphaIndex] = fetch(first).insert(string.rest)
-            return this
-        }
-        return WordParentNode(chars)
-    }
-
-    override val name: String = "OnlyParent"
-
-    override val isWord: Boolean = false
-}
-
-/**
- * A trie node that represents a valid word that is NOT a prefix for any other words
- */
-object OnlyWordNode : TrieNode() {
-    override fun fetch(char: Char): TrieNode = NullNode
-
-    override fun insert(string: String): TrieNode {
-        if (string.isEmpty()) return this
-        val first = string.first()
-        val newParent = WordParentNode()
-        if (string.length == 1) {
-            newParent.insertChar(first)
-        } else {
-            newParent.insert(string)
-        }
-        return newParent
-    }
-
-    override fun toString(): String = "ChildNode"
-
-    override val nodeSize: Int = 0
-}
-
-/**
- * A trie node that is neither a word, or a prefix for a word. A dead end.
- */
-object NullNode : TrieNode() {
-    override fun fetch(char: Char): TrieNode = NullNode
-
-    override fun insert(string: String): TrieNode {
-        if (string.isEmpty()) {
-            return OnlyWordNode
-        }
-        return OnlyParentNode().insert(string)
-    }
-
-    override fun toString(): String = "Null"
-
-    override val nodeSize: Int = 0
-}
-
-/**
- * Convert UInt into a byte array
- */
-fun UInt.toBytes(isBigEndian: Boolean = true, wordLength: Int = ADDRESS_LENGTH_BYTES): List<Byte> {
-    val bytes = mutableListOf<Byte>()
-    var n = this
-    if (n == 0x00u) {
-        bytes += n.toByte()
-    } else {
-        while (n != 0x00u) {
-            bytes += n.toByte()
-            n = n.shr(Byte.SIZE_BITS)
+fun URL.downloadFileTo(file: File) {
+    openStream().use { inp ->
+        BufferedInputStream(inp).use { bis ->
+            file.outputStream().use { fos ->
+                val data = ByteArray(1024)
+                var count: Int
+                while (bis.read(data, 0, 1024).also { count = it } != -1) {
+                    fos.write(data, 0, count)
+                }
+            }
         }
     }
-
-    val paddings = if (bytes.size > wordLength) {
-        emptyList()
-    } else {
-        List<Byte>(wordLength - bytes.size) { 0x00 }
-    }
-    return paddings + if (isBigEndian) bytes.reversed() else bytes
 }
 
